897fd41314ab939f498b5ff3ecaccf3ff03b67ed
jon2-vera-cruz <jon2_vera_cruz@hotmail.com>
2021-04-30 22:09:43 +0000

    Add google map

--- static/js/script.js
+++ static/js/script.js
@@ -40,3 +40,19 @@ $(document).ready(function(){
         });
     }
 });
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 53.38220, lng: -6.27648 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
